d22f29c2b0ca1b51fa2591b334b563b90e9389b4
Christine Nguyen <ctine.ngyn@gmail.com>
2021-06-12 00:24:31 -0700

    updating tags name by id value

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -40,10 +40,17 @@ router.post('/', (req, res) => {
   Tag.create(req.body)
   .then((tag) => res.status(200).json(tag))
   .catch((err) => res.status(404).json(err));
-  // create a new tag
+  // create a new tag 
 });
 
 router.put('/:id', (req, res) => {
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((tag) => res.status(200).json(tag))
+  .catch((err) => res.status(404).json(err));
   // update a tag's name by its `id` value
 });
 
