facebf92c68bd172403a17d0f16d6c77fbe984de
Christine Nguyen <ctine.ngyn@gmail.com>
2021-06-12 00:27:14 -0700

    delete tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -55,6 +55,13 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((tag) => res.status(200).json(tag))
+  .catch((err) => res.status(404).json(err));
   // delete on tag by its `id` value
 });
 
