1157672820b84dedef7a4db69a2d9a929c83c2e1
Christine Nguyen <ctine.ngyn@gmail.com>
2021-06-12 00:17:39 -0700

    router post

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -37,6 +37,9 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
+  Tag.create(req.body)
+  .then((tag) => res.status(200).json(tag))
+  .catch((err) => res.status(404).json(err));
   // create a new tag
 });
 
